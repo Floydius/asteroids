07bfe2f07f8781cae756d5488cdeca683d3dc3e8
Floydius <goodspam@floydius.com>
2025-10-05 21:46:34 -0500

    added player object and draw method to loop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,13 +12,18 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    new_player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        new_player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
+        
+
 
 if __name__ == "__main__":
     main()
