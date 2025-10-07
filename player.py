ab8c5fa2d92d11c4e4e3a4bc749cb7faab746964
Floydius <goodspam@floydius.com>
2025-10-06 22:24:27 -0500

    added rotate and update methods

--- player.py
+++ player.py
@@ -16,4 +16,17 @@ class Player(CircleShape):
         return [a, b, c]
     
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        self.screen = screen
+        pygame.draw.polygon(self.screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.dt = dt
+        self.rotation += (PLAYER_TURN_SPEED * self.dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate((dt * -1))
